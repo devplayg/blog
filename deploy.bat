151a2fbb60bc19066251dad285f4c5bdcd15ed4d
devplayg <devplayg@gmail>
2021-08-22 11:16:57 +0900

    fixed

--- deploy.bat
+++ deploy.bat
@@ -2,11 +2,11 @@ hugo -t hyde-hyde
 
 cd public
 git add .
-git commit -m "changed"
-git push
+git commit * -m "changed"
+git push origin master
 
 cd ..
 git add .
-git commit -m "changed"
-git push
+git commit * -m "changed"
+git push origin master
 
